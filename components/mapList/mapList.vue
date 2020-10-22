ccbed2b8158930a82d7c13967842b19feb6c7668
whutfaker <1312976124@qq.com>
2020-10-22 21:57:45 +0800

    修改部分代码

---
+++ components/mapList/mapList.vue
@@ -0,0 +1,22 @@
+<template>
+	<view class="class-box">
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style scoped>
+.class-box{
+	display: flex;
+	flex-direction: row;
+}
+</style>
