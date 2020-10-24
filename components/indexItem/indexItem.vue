0bfb8e6601caa7e5e235d042651ce6df5786b817
khannb <838278270@qq.com>
2020-10-24 14:52:57 +0800

    首页bug

--- components/indexItem/indexItem.vue
+++ components/indexItem/indexItem.vue
@@ -1,21 +1,28 @@
 <template>
-	<view class="contains">
-		<!-- <view  v-if="!onlySwiper" class="contains-button">{{ buttonTitle }}</view> -->
-		<swiper indicator-color="white" indicator-active-color="#FFD66C" :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000">
-			<swiper-item v-for="(item, idx) in picture">
-				<view class="swiper-item">
-					<image :src="item.src" mode="widthFix" ></image>
-					<!-- <view style="color: #1D3245; width: 90%; height: 30upx; "></view> -->
+	<view class="indexItem-contains" @mouseenter.native="changebtn" @mouseleave.native="changebtn">
+		<view style="background-color: #001418; margin: 0 auto; width: 90%;">
+			<!-- <view  v-if="!onlySwiper" class="contains-button">{{ buttonTitle }}</view> -->
+			<swiper indicator-color="white" indicator-active-color="#FFD66C" :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000">
+				<swiper-item v-for="(item, idx) in picture" :key="idx">
+					<view class="swiper-item">
+						<image :src="item.src" mode="widthFix" ></image>
+						<!-- <view style="color: #1D3245; width: 90%; height: 30upx; "></view> -->
+					</view>
+					
+				</swiper-item>
+			</swiper>
+			<view class="contains-small" v-if="!onlySwiper" >
+				<view class="contains-Namebutton" :class="{active : hover}">
+				<view style="margin-left: 100upx; padding-top: 0; color: white; font-family: STYuanti;">
+					{{ gameName }}
 				</view>
-				
-			</swiper-item>
-		</swiper>
-		<view class="contains-small" v-if="!onlySwiper" >
-			<view class="contains-description" style="color: white">{{ gameName }}</view>
-			<image src="../../static/index/toDetail.png" style="width: 75upx; height: 75upx;"></image>
+					<image :src="btnUrl"   style="margin-top: -10upx; width: 75upx; height: 75upx; "></image>
+				</view>
+				<view style="color: white; font-size: small; font-family: STYuanti;">介绍: {{describe}}</view>
+			</view>
+			
 		</view>
-		<view style="color: white;">介绍: {{describle}}</view>
-		<view class="line-bottom"></view>
+		
 	</view>
 </template>
 
@@ -25,8 +32,9 @@ export default {
 		picture: {
 			type: Array // {{src:ddd,id:ddd}}
 		},
-		buttonTitle: {
-			type: String
+		describe:{
+			type: String,
+			default: ""
 		},
 		gameName: {
 			type: String
@@ -37,30 +45,40 @@ export default {
 		}
 	},
 	data() {
-		return {};
+		return {
+			"btnUrl":'../../static/index/toDetail.png',
+			"hover":false
+		};
 	},
 	mounted() {
 		console.log(this.picture[0]);
-	}
-};
+		console.log(this.describe)
+	},
+	methods:{
+		changebtn(){
+			this.hover= !this.hover
+			this.btnUrl = (this.hover===true)? '../../static/index/toDetailClicked.png' : '../../static/index/toDetail.png'
+		}
+	},
+}
 </script>
 
 <style scoped>
-.
-	
-	
-.contains {
+.indexItem-contains {
 	display: flex;
 	flex-direction: column;
 	justify-content: center;
 	align-content: center;
 	text-align: center;
-	width: 90%;
+	width: 100%;
 	background-color: #00191E;
 }
 .contains > * {
 	margin-top: 30upx;
 }
+
+
+
 .contains-button {
 	background-color: #031113;
 	width: 30%;
@@ -77,25 +95,30 @@ export default {
 	margin: 10upx 80upx;
 	/* width: 85%; */
 	display: flex;
-	flex-direction: row;
+	flex-direction: column;
 	justify-content: center;
 	align-items: center;
 	text-align: center;
-	background-color: #0D2331;
+	background-color: #001418;
 	border-radius: 20upx;
 }
-.contains-description {
-	/* background-color: #0d2330; */
-	width: 35%;
-	height: 50upx;
+
+.contains-Namebutton {
+	/* padding-bottom: 0; */
+	display: flex;
+	flex-direction: row;
+	justify-content: space-evenly;
+	text-align: center;
+	background-color: #0d2331;
+	width: 70%;
+	height: 60upx;
 	line-height: 50upx;
 	border-radius: 30upx;
+	margin: 10upx;
 }
 
-.line-bottom {
-		margin: 10upx auto;
-		width: 70%;
-		height: 1px;
-		border-top: solid #A6C0D6 5upx;
+.active{
+	background-color: #444D4A;
 }
+
 </style>
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,9 +1,22 @@
 <template>
 	<view class="contains">
-		<view></view>
-		<view class="list-contains" v-for="item in swipers" :key="gameItem">
-			<indexItem :picture="item.picture" :describe="item.describe" :gameName="item.gameName" />
+		<!-- 导航栏 -->
+		<view class="nav-bar">
+			<text class="nav-bar-item" :class="{ 'current-item': currentPlace == 0 }" @click="currentPlace = 0">端游</text>
+			<text class="nav-bar-item" :class="{ 'current-item': currentPlace == 1 }" @click="currentPlace = 1">手游</text>
 		</view>
+		<swiper class="swiper" :duration="200" @change="swiperChange" :current="currentPlace">
+			<swiper-item class="backg">
+					<view class="list-contains" v-for="(item,index) in swipers"  v-bind:key="item.gameName">
+						<indexItem :picture="item.picture" :describe="item.describe" :gameName="item.gameName"  />
+						<!-- 加判断，使得最后一个没有底栏 -->
+						<view class="line-bottom" v-if="index!==swipers.length-1"></view>
+					</view>
+			</swiper-item>
+			<swiper-item class="swiper-item"></swiper-item>
+		</swiper>
+		
+		
 	</view>
 	
 </template>
@@ -19,7 +32,8 @@
 		},
 		data() {
 			return {
-				gameItem:"gameItem",
+				
+				currentPlace: 0,
 				swipers:[
 					{
 						picture:[
@@ -40,7 +54,73 @@
 							id:12
 						}
 						],
-						describe:"xxxxxxxxxx",
+						describe: "位于提瓦特大陆东方的富饶港湾。傲然矗立的山麓与石林、广袤的平原与生机勃勃的河滩共同构成了璃月的丰富地貌，在四季分明的气候下焕发出多彩的风华。山石奇景间，又埋藏了多少岩之魔神的古老馈赠等待着人们发掘呢？",
+						gameName:"原神",
+					},
+					{
+						picture:[
+						{
+							src:"https://img.xjh.me/random_img.php?type=bg&ctype=nature&return=302",
+							id:12
+						},
+						{
+							src:"https://img.xjh.me/random_img.php?type=bg&ctype=nature&return=302",
+							id:12
+						},
+						{
+							src:"https://img.xjh.me/random_img.php?type=bg&ctype=nature&return=302",
+							id:12
+						},
+						{
+							src:"https://img.xjh.me/random_img.php?type=bg&ctype=nature&return=302",
+							id:12
+						}
+						],
+						describle: "xxxxxxxxxx",
+						gameName:"糖豆人",
+					},
+					{
+						picture:[
+						{
+							src:"https://img.xjh.me/random_img.php?type=bg&ctype=nature&return=302",
+							id:12
+						},
+						{
+							src:"https://img.xjh.me/random_img.php?type=bg&ctype=nature&return=302",
+							id:12
+						},
+						{
+							src:"https://img.xjh.me/random_img.php?type=bg&ctype=nature&return=302",
+							id:12
+						},
+						{
+							src:"https://img.xjh.me/random_img.php?type=bg&ctype=nature&return=302",
+							id:12
+						}
+						],
+						describe: "xxxxxxxxxx",
+						gameName:"组合名称3",
+					},
+					{
+						picture:[
+						{
+							src:"https://img.xjh.me/random_img.php?type=bg&ctype=nature&return=302",
+							id:12
+						},
+						{
+							src:"https://img.xjh.me/random_img.php?type=bg&ctype=nature&return=302",
+							id:12
+						},
+						{
+							src:"https://img.xjh.me/random_img.php?type=bg&ctype=nature&return=302",
+							id:12
+						},
+						{
+							src:"https://img.xjh.me/random_img.php?type=bg&ctype=nature&return=302",
+							id:12
+						}
+						],
+						describe: "xxxxxxxxxx",
 						gameName:"组合名称",
 					},
 					{
@@ -62,80 +142,17 @@
 							id:12
 						}
 						],
-						describe:"xxxxxxxxxx",
-						gameName:"组合名称",
-					},
-					{
-						picture:[
-						{
-							src:"https://img.xjh.me/random_img.php?type=bg&ctype=nature&return=302",
-							id:12
-						},
-						{
-							src:"https://img.xjh.me/random_img.php?type=bg&ctype=nature&return=302",
-							id:12
-						},
-						{
-							src:"https://img.xjh.me/random_img.php?type=bg&ctype=nature&return=302",
-							id:12
-						},
-						{
-							src:"https://img.xjh.me/random_img.php?type=bg&ctype=nature&return=302",
-							id:12
-						}
-						],
-						describe:"xxxxxxxxxx",
-						gameName:"组合名称",
-					},
-					{
-						picture:[
-						{
-							src:"https://img.xjh.me/random_img.php?type=bg&ctype=nature&return=302",
-							id:12
-						},
-						{
-							src:"https://img.xjh.me/random_img.php?type=bg&ctype=nature&return=302",
-							id:12
-						},
-						{
-							src:"https://img.xjh.me/random_img.php?type=bg&ctype=nature&return=302",
-							id:12
-						},
-						{
-							src:"https://img.xjh.me/random_img.php?type=bg&ctype=nature&return=302",
-							id:12
-						}
-						],
-						describe:"xxxxxxxxxx",
-						gameName:"组合名称",
-					},
-					{
-						picture:[
-						{
-							src:"https://img.xjh.me/random_img.php?type=bg&ctype=nature&return=302",
-							id:12
-						},
-						{
-							src:"https://img.xjh.me/random_img.php?type=bg&ctype=nature&return=302",
-							id:12
-						},
-						{
-							src:"https://img.xjh.me/random_img.php?type=bg&ctype=nature&return=302",
-							id:12
-						},
-						{
-							src:"https://img.xjh.me/random_img.php?type=bg&ctype=nature&return=302",
-							id:12
-						}
-						],
-						describe:"xxxxxxxxxx",
-						gameName:"组合名称",
+						describe: "xxxxxxxxxx",
+						gameName:"组合名称4",
 					}
 				]
 			}
 		},
 		methods: {
-			
+			swiperChange(e){
+				console.log(e.detail.current);
+				this.currentPlace = e.detail.current;
+			}
 		}
 	}
 </script>
@@ -146,5 +163,64 @@
 	flex-direction: column;
 	text-align: center;
 	justify-content: center;
+}
+
+/* swiper */
+swiper {
+	width: 100%;
+	height: 100vh;
+	overflow: scroll;
+}
+
+.swiper-item {
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	text-align: center;
+	overflow: scroll;
+}
+
+/* 导航栏 */
+.nav-bar {
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+	align-items: center;
+	text-align: center;
+	width: 100vw;
+	font-size: 15px;
+	font-weight: 900;
+	padding-top: 24upx;
+	height: 9vh;
+	color: #fff;
+	background-color: #112628;
+	position: sticky;
+	top: 0;
+	z-index: 1;
+}
+.nav-bar-item {
+	padding: 0 40upx;
+}
+.current-item {
+	font-size: 20px;
+}
+
+.line-bottom {
+		margin: 10upx auto;
+		width: 70%;
+		height: 1px;
+		border-top: solid #A6C0D6 3upx;
+}
+
+.backg{
+	/* margin: 10upx 80upx; */
+	width: 100%;
+	/* display: flex;
+	flex-direction: column;5
+	text-align: center; */
+	background-color: #001418;
+	/* linear-gradient(to bottom, #16242A,#001418) */
+	/* border-radius: 20upx; */
 }
 </style>
