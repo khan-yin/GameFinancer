0b4a33ef95a9be39389b563553c4055cad261787
whutfaker <1312976124@qq.com>
2020-10-27 12:40:50 +0800

    finished login&profile pages' api

--- pages/Login/Login.vue
+++ pages/Login/Login.vue
@@ -1,61 +1,56 @@
 <template>
 	<view class="index-contain">
-		<view class="index-body">
-			<image src="../../static/common-icons/playLogo.png" class="cell-pic" mode="widthFix"></image>
-		</view>
-		
+		<view class="index-body"><image src="../../static/common-icons/playLogo.png" class="cell-pic" mode="widthFix"></image></view>
 		<view class="login-contain" v-if="status">
 			<view class="inputitem">
-			<text>邮箱：</text>
-			<input class="inputstyle" type="text" value="" />
+				<text>邮箱：</text>
+				<input class="inputstyle" type="text" v-model="email" />
 			</view>
 			<view class="inputitem">
-			<text>密码：</text>
-			<input class="inputstyle" type="password" value="" />
+				<text>密码：</text>
+				<input class="inputstyle" type="password" v-model="password" />
 			</view>
 			<view class="button-contains">
 				<button type="default" class="btnstyle" @click="doLogin">登录</button>
-				<button type="default" class="btnstyle"  @click="change">注册</button>
+				<button type="default" class="btnstyle" @click="change">注册</button>
 			</view>
 		</view>
-		<view class="sign-contain"  v-else>
+		<view class="sign-contain" v-else>
 			<view class="inputitem">
 				<text>用户名：</text>
-				<input class="inputstyle" type="text" value="" />
+				<input v-model="username" class="inputstyle" type="text" />
 			</view>
-			
+
 			<view class="inputitem">
-			<text>名字：</text>
-			<input class="inputstyle" type="text" value="" />
+				<text>名字：</text>
+				<input v-model="name" class="inputstyle" type="text" value="" />
 			</view>
-			
+
 			<view class="inputitem">
-			<text>姓氏：</text>
-			<input class="inputstyle" type="text" value="" />
+				<text>姓氏：</text>
+				<input v-model="lastName" class="inputstyle" type="text" value="" />
 			</view>
-			
+
 			<view class="inputitem">
-			<text>电子邮箱：</text>
-			<input class="inputstyle" type="text" value="" />
+				<text>电子邮箱：</text>
+				<input v-model="email" class="inputstyle" type="text" value="" />
 			</view>
-			
+
 			<view class="inputitem">
-			<text>密码：</text>
-			<input class="inputstyle" type="password" value="" />
+				<text>密码：</text>
+				<input v-model="password" class="inputstyle" type="password" value="" />
 			</view>
-			
+
 			<view class="inputitem">
-			<text>确认密码：</text>
-			<input class="inputstyle" type="password" value="" />
+				<text>确认密码：</text>
+				<input v-model="confirmPassword" class="inputstyle" type="password" value="" />
 			</view>
-			
+
 			<view class="button-contains">
 				<button class="btnstyle" @click="change">返回</button>
 				<button class="btnstyle" @click="doSignup">注册</button>
 			</view>
 		</view>
-		
-		
 	</view>
 </template>
 
@@ -63,7 +58,13 @@
 export default {
 	data() {
 		return {
-			status:true,
+			status: true,
+			username: 'root',
+			password: '123456',
+			name: 'x',
+			lastName: 'a',
+			email: 'codingax@foxmail.com',
+			confirmPassword: '123456'
 		};
 	},
 	methods: {
@@ -73,22 +74,152 @@ export default {
 			});
 		}
 	},
-	methods:{
-		change(){
-			this.status=!this.status;
+	methods: {
+		change() {
+			this.status = !this.status;
+			uni.setNavigationBarTitle({
+				title: this.status ? '登录' : '注册'
+			});
+			console.log(this.status);
 		},
-		doLogin(){
-			
+		async doLogin() {
+			console.log(this.email, this.password);
+			// 验证输入
+			if (this.email && this.password) {
+				uni.showLoading({
+					title: '登录中',
+					content: '请耐心等待',
+					mask: true
+				});
+				// 发起请求
+				let res = uni.request({
+					url: 'http://server.kingfish404.cn/main/signIn',
+					method: 'POST',
+					timeout: 3000,
+					header: {
+						'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+					},
+					data: {
+						email: this.email,
+						password: this.password,
+						remember_me: 'on'
+					},
+					success(res) {
+						uni.hideLoading();
+						console.log(res)
+						// 打印一下然后准备存起来 jwt
+						if (res.data.code == 0) {
+							console.log(res)
+							console.log(res.header.Authorization);
+							uni.setStorageSync('authorization', res.header.Authorization);
+							uni.showToast({
+								title: '登录成功',
+								duration: 1000
+							});
+							setTimeout(() => {
+								uni.switchTab({
+									url: '../profile/profile?login=true'
+								});
+							}, 1000);
+						} else {
+							uni.showModal({
+								title: '提示',
+								content: '密码或账号错误！',
+								showCancel: false
+							});
+						}
+					},
+					fail() {
+						uni.hideLoading();
+						uni.showModal({
+							title: '提示',
+							content: '网络出了点小差！',
+							showCancel: false
+						});
+					}
+				});
+			} else {
+				uni.showModal({
+					title: '提示',
+					content: '请输入完整的账号和密码！',
+					showCancel: false
+				});
+			}
 		},
-		doSignup(){
-			
+		doSignup() {
+			console.log(this.email, this.password);
+			if (this.email && this.password && this.lastName && this.name && this.confirmPassword && this.username) {
+				if (this.password != this.confirmPassword) {
+					uni.showToast({
+						duration: 1000,
+						title: '两次密码输入不一致',
+						icon: 'none'
+					});
+					return;
+				}
+				uni.showLoading({
+					title: '注册中',
+					content: '请耐心等待',
+					mask: true
+				});
+				let data = {
+					email: this.email,
+					password1: this.password,
+					first_name: this.lastName,
+					last_name: this.name,
+					password2: this.confirmPassword,
+					username: this.username
+				};
+				console.log(data);
+				let res = uni.request({
+					url: 'http://server.kingfish404.cn/main/signUp',
+					method: 'POST',
+					timeout: 3000,
+					header: {
+						'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+					},
+					data,
+					success:(res)=> {
+						uni.hideLoading();
+						if(res.data.code==0){
+							uni.showToast({
+								title:"注册成功",
+								duration:1000
+							})
+							setTimeout(()=>{
+								this.change();
+							},1000)
+						}else{
+							uni.hideLoading();
+							uni.showModal({
+								title: '提示',
+								content: '当前邮箱已存在',
+								showCancel: false
+							});
+						}
+					},
+					fail:()=> {
+						uni.hideLoading();
+						uni.showModal({
+							title: '提示',
+							content: '网络出了点小差！',
+							showCancel: false
+						});
+					}
+				});
+			} else {
+				uni.showModal({
+					title: '提示',
+					content: '请输入完整的账号和密码！',
+					showCancel: false
+				});
+			}
 		}
 	}
 };
 </script>
 
 <style scoped>
-
 .index-contain {
 	background-image: url(../../static/background/index.png);
 	background-size: 100%;
@@ -112,11 +243,11 @@ export default {
 }
 .cell-pic {
 	width: 70%;
-/* 	position: relative;
+	/* 	position: relative;
 	top: -400upx; */
 }
 
-.login-contain{
+.login-contain {
 	align-items: center;
 	justify-content: center;
 	width: 90%;
@@ -128,8 +259,7 @@ export default {
 	opacity: 0.6;
 }
 
-
-.sign-contain{
+.sign-contain {
 	align-items: center;
 	justify-content: center;
 	width: 90%;
@@ -141,7 +271,7 @@ export default {
 	opacity: 0.6;
 }
 
-.inputitem{
+.inputitem {
 	display: flex;
 	flex-direction: row;
 	justify-content: space-between;
@@ -154,8 +284,8 @@ export default {
 	color: #000000;
 }
 
-.inputstyle{
-	background-color: #EBEBEB;
+.inputstyle {
+	background-color: #ebebeb;
 	height: 70upx;
 	width: 70%;
 	border: solid 5upx #000000;
@@ -164,7 +294,7 @@ export default {
 	text-align: left;
 }
 
-.button-contains{
+.button-contains {
 	display: flex;
 	flex-direction: row;
 	justify-content: space-between;
@@ -172,7 +302,7 @@ export default {
 	text-align: center;
 }
 
-.btnstyle{
+.btnstyle {
 	width: 200upx;
 	height: 50upx;
 	background-color: #ffc680;
--- pages/profile/profile.vue
+++ pages/profile/profile.vue
@@ -2,56 +2,32 @@
 	<view class="contains">
 		<view class="userCard">
 			<!-- 头像 -->
-			<view>
-				<image  class="ImgClass" :src="userimgUrl" mode="widthFix"></image>
-			</view>
+			<view><image @click="chooseImg" class="ImgClass" :src="userimgUrl" mode="widthFix"></image></view>
 			<!-- 登录后显示名称，id -->
 			<view v-if="isLogin" class="LoginInfo">
 				<view class="user-name">
 					<image class="icon" src="../../static/profile/usericon.png"></image>
-					<view style="font-size: 40upx;">-
-						{{username}}
-					</view>
+					<view style="font-size: 40upx;">- {{ username }}</view>
 				</view>
 
-				<view class="idstyle">
-					game financer ID:{{userId}}
-				</view>
+				<view class="idstyle">game financer ID:{{ userId }}</view>
 			</view>
 
-			<view v-else class="LoginInfo" @click="navTo('../Login/Login')">
-				登录
-			</view>
-
+			<view v-else class="LoginInfo" @click="navTo('../Login/Login')">登录</view>
 		</view>
 
 		<view class="box">
-			<view class="box-item" >
-				账户
-			</view>
+			<view class="box-item">账户</view>
 
-			<view class="box-item" style="background-color: #24393F;">
-				我的持债
-			</view>
+			<view class="box-item" style="background-color: #24393F;">我的持债</view>
 
-			<view class="box-item" style="background-color: #192E34;">
-				众筹订单
-			</view>
-
+			<view class="box-item" style="background-color: #192E34;">众筹订单</view>
 		</view>
 
-
-
 		<view class="box">
-			<view class="box-item">
-				道具订单
-			</view>
-			<view class="box-item" style="background-color: #24393F;">
-				周边订单
-			</view>
-			<view class="box-item">
-				我的游戏账号
-			</view>
+			<view class="box-item">道具订单</view>
+			<view class="box-item" style="background-color: #24393F;">周边订单</view>
+			<view class="box-item">我的游戏账号</view>
 		</view>
 
 		<view class="box">
@@ -59,123 +35,166 @@
 			<view class="box-item" style="background-color: #24393F;">我制作的项目</view>
 			<view class="box-item">设置</view>
 		</view>
-
-
-
 	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				isLogin: false,
-				username: "血小板",
-				userId: "0121810880322",
-				userimgUrl: "../../static/profile/userImg.png",//"https://khany.top/assets/xxb.jpg"//
+export default {
+	data() {
+		return {
+			authorization: '',
+			isLogin: false,
+			username: '血小板',
+			userId: '0121810880322',
+			userimgUrl: '../../static/profile/userImg.png' //"https://khany.top/assets/xxb.jpg"//
+		};
+	},
+	methods: {
+		navTo(url) {
+			console.log(url);
+			if (url) {
+				uni.navigateTo({
+					url: url,
+					animationType: 'slide-in-right',
+					animationDuration: 200
+				});
 			}
 		},
-		methods: {
-			navTo(url) {
-				console.log(url);
-				if (url) {
-					uni.navigateTo({
-						url: url,
-						animationType: 'slide-in-right',
-						animationDuration: 200
+		// 头像设置
+		chooseImg() {
+			uni.chooseImage({
+				count: 1,
+				success: res => {
+					console.log(res.tempFiles[0]);
+					this.userimgUrl = res.tempFiles[0].path;
+					uni.saveFile({
+						tempFilePath: this.userimgUrl,
+						success: res => {
+							console.log(res.savedFilePath);
+							this.userimgUrl = res.savedFilePath;
+							uni.setStorageSync('userimgUrl', this.userimgUrl);
+						},
+						fail: err => {
+							console.log(err);
+						}
 					});
 				}
-			}
-		},
-
+			});
+		}
+	},
+	onShow() {
+		let Authorization = uni.getStorageSync('authorization');
+		console.log(Authorization);
+		if (Authorization) {
+			this.authorization = Authorization;
+			uni.request({
+				url: 'http://server.kingfish404.cn/main/getUserInfo',
+				method: 'POST',
+				header: {
+					Authorization
+				},
+				success: res => {
+					console.log(res);
+					this.isLogin = true;
+					if (res.data.code == 0) {
+						this.username = res.data.username;
+						this.userId = res.data.useremail;
+						this.userimgUrl = uni.getStorageSync('userimgUrl');
+						if (!this.userimgUrl) {
+							this.userimgUrl = '../../static/profile/userImg.png';
+						}
+					}else{
+						this.navTo('../Login/Login')
+					}
+				}
+			});
+		}
 	}
+};
 </script>
 
 <style>
-	.contains {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		text-align: center;
+.contains {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	text-align: center;
+}
 
-	}
+.idstyle {
+	margin-top: 40upx;
+	background-color: #20373b;
+	color: #ffffff;
+	padding: 10upx;
+	border-radius: 5upx;
+	font-size: 25upx;
+}
 
-	.idstyle {
-		margin-top: 40upx;
-		background-color: #20373B;
-		color: #FFFFFF;
-		padding: 10upx;
-		border-radius: 5upx;
-		font-size: 25upx;
-		
-	}
+.icon {
+	width: 50upx;
+	height: 50upx;
+}
 
-	.icon {
-		width: 50upx;
-		height: 50upx;
-	}
+.userCard {
+	display: flex;
+	flex-direction: row;
+	text-align: center;
+}
 
-	.userCard {
-		display: flex;
-		flex-direction: row;
-		text-align: center;
-	}
+.ImgClass {
+	position: relative;
+	top: 30upx;
+	height: 250upx;
+	width: 250upx;
+	border-radius: 50%;
+	opacity: 0.7;
+	box-shadow: 0 0 70upx rgb(69, 81, 91);
+}
 
-	.ImgClass {
-		position: relative;
-		top: 30upx;
-		height: 250upx;
-		width: 250upx;
-		border-radius: 50%;
-		opacity:0.7;
-		box-shadow: 0 0 70upx rgb(69, 81, 91);
-	}
+.LoginInfo {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	width: 430upx;
+	height: 300upx;
+	vertical-align: middle;
+	align-items: center;
+	font-size: 80upx;
+}
 
-	.LoginInfo {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		width: 430upx;
-		height: 300upx;
-		vertical-align: middle;
-		align-items: center;
-		font-size: 80upx;
-	}
+.user-name {
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+	align-items: center;
+	text-align: center;
+}
 
-	.user-name {
-		display: flex;
-		flex-direction: row;
-		justify-content: center;
-		align-items: center;
-		text-align: center;
-	}
+.user-name > * {
+	padding: 6upx;
+}
 
-	.user-name>* {
-		padding: 6upx;
-	}
+.box {
+	border-radius: 10upx;
+	margin: 10upx;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	height: 200upx;
+	width: 600upx;
+	/* background-color: #007AFF; */
+	/* border: solid 1upx #007AFF; */
+	box-shadow: 0upx 0upx 5upx 5upx #0f272b;
+}
 
-	.box {
-		border-radius: 10upx;
-		margin: 10upx;
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		height: 200upx;
-		width: 600upx;
-		/* background-color: #007AFF; */
-		/* border: solid 1upx #007AFF; */
-		box-shadow: 0upx 0upx 5upx 5upx #0F272B;
-	}
-
-
-	.box-item {
-		text-align: left;
-		vertical-align: middle;
-		height: 34%;
-		width: 95%;
-		padding-left: 5%;
-		background-color: #152A30;
-		line-height: 66upx;
-	}
+.box-item {
+	text-align: left;
+	vertical-align: middle;
+	height: 34%;
+	width: 95%;
+	padding-left: 5%;
+	background-color: #152a30;
+	line-height: 66upx;
+}
 </style>
